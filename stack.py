f3e933f546d97cb232b8c29bb45d813ef1d9701c
Rustam <43505186+rustam66637@users.noreply.github.com>
2019-04-09 00:59:23 +0500

    create new file

---
+++ stack.py
@@ -0,0 +1,17 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def size(self):
+        return len(self.stack)
+
+    def pop(self):
+        # ваш код
+        return None # если стек пустой
+
+    def push(self, value):
+        # ваш код
+
+    def peek(self):
+        # ваш код
+        return None # если стек пустой
